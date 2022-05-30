7e4899fa6006a75926d8783c91dcb52a82690e94
seba <sebamgb@gmail.com>
2022-05-30 14:30:07 -0400

    switch

--- src/calculadora.go
+++ src/calculadora.go
@@ -13,16 +13,29 @@ func main() {
 	scanner.Scan()
 	operation := scanner.Text()
 	fmt.Println(operation)
-	valores := strings.Split(operation, "+")    //lista de strings; cada indice son strings en operation sin el + ya que se usa como separado
-	fmt.Println(valores[0] + valores[1])        //concatenación de strings en valores
-	operator1, err1 := strconv.Atoi(valores[0]) //string valores[0] convertido a entero
+	operator := "p"
+	valores := strings.Split(operation, operator) //lista de strings; cada indice son strings en operation sin el + ya que se usa como separado
+	fmt.Println(valores[0] + valores[1])          //concatenación de strings en valores
+	factor1, err1 := strconv.Atoi(valores[0])     //string valores[0] convertido a entero
 	if err1 != nil {
 		fmt.Println(err1)
 	}
-	operator2, err2 := strconv.Atoi(valores[1]) //string valores[1] convertido a entero
+	factor2, err2 := strconv.Atoi(valores[1]) //string valores[1] convertido a entero
 	if err2 != nil {
 		fmt.Println(err2)
 	}
-	suma := operator1 + operator2
-	fmt.Println(suma)
+	var result int
+	switch operator {
+	case "+":
+		result = factor1 + factor2
+	case "-":
+		result = factor1 - factor2
+	case "*":
+		result = factor1 * factor2
+	case "/":
+		result = factor1 / factor2
+	default:
+		fmt.Printf("invalid operator '%v'\n", operator)
+	}
+	fmt.Println(result)
 }
