ae3b29f05fdd906a46093dfce175ee61aaad48e7
seba <sebamgb@gmail.com>
2022-05-30 13:26:05 -0400

    manejando errores

--- src/calculadora.go
+++ src/calculadora.go
@@ -13,10 +13,16 @@ func main() {
 	scanner.Scan()
 	operation := scanner.Text()
 	fmt.Println(operation)
-	valores := strings.Split(operation, "+") //lista de strings; cada indice son strings en operation sin el + ya que se usa como separado
-	fmt.Println(valores[0] + valores[1])     //concatenación de strings en valores
-	operator1, _ := strconv.Atoi(valores[0]) //string valores[0] convertido a entero
-	operator2, _ := strconv.Atoi(valores[1]) //string valores[1] convertido a entero
+	valores := strings.Split(operation, "+")    //lista de strings; cada indice son strings en operation sin el + ya que se usa como separado
+	fmt.Println(valores[0] + valores[1])        //concatenación de strings en valores
+	operator1, err1 := strconv.Atoi(valores[0]) //string valores[0] convertido a entero
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	operator2, err2 := strconv.Atoi(valores[1]) //string valores[1] convertido a entero
+	if err2 != nil {
+		fmt.Println(err2)
+	}
 	suma := operator1 + operator2
 	fmt.Println(suma)
 }
