e935dddeddea37ae108ec94f55d9047152ed24aa
seba <sebamgb@gmail.com>
2022-06-04 21:31:34 -0400

    multiples middlewares

--- web-server/middleware.go
+++ web-server/middleware.go
@@ -2,19 +2,36 @@ package webserver
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func CheckAuth() Middleware {
 	return func(hf http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			var flag bool = true
-			fmt.Println("Checking authentication")
+			fmt.Println("'Checking authentication'")
 			if flag {
 				hf(w, r)
+				fmt.Println("success authentication")
 			} else {
+				fmt.Println("access denied")
 				return
+
 			}
+		}
+	}
+}
+
+func Logging() Middleware {
+	return func(hf http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() {
+				log.Println(r.URL.Path, time.Since(start))
+			}()
+			hf(w, r)
 		}
 	}
 }
--- web-server/web-server.go
+++ web-server/web-server.go
@@ -2,7 +2,7 @@ package webserver
 
 func Web_server_main() {
 	server := NewServer(":3000")
-	server.Handle("/", HandleRoot)
-	server.Handle("/api", server.AddMiddleware(HandleApi, CheckAuth()))
+	server.Handle("/", server.AddMiddleware(HandleRoot, Logging()))
+	server.Handle("/api", server.AddMiddleware(HandleApi, Logging(), CheckAuth()))
 	server.Listen()
 }
